47879a0859002608dafea7adab2656dd8a8bb1f2
Nizar Haider <nizarhaider@gmail.com>
2024-11-21 23:02:01 +0530

    stupid site published

--- src/components/react/carosel.jsx
+++ src/components/react/carosel.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-import { Navigation, FreeMode, Thumbs, Mousewheel, Autoplay, Pagination } from "swiper/modules";
+
+// import required modules
+import { Pagination } from 'swiper/modules';
 
 export default function SwiperCard() {
   return (
-    <div className="h-screen bg-gray-200 flex items-center justify-center">
-      <Swiper
-        slidesPerView={'auto'}
-        spaceBetween={30}
-        pagination={{ clickable: true }}
-        modules={[Pagination]}
-        className="w-full h-full"
-      >
-        <SwiperSlide className="flex justify-center items-center w-4/5 h-48 bg-white text-lg">Slide 1</SwiperSlide>
-        <SwiperSlide className="flex justify-center items-center w-3/5 h-48 bg-white text-lg">Slide 2</SwiperSlide>
-        <SwiperSlide className="flex justify-center items-center w-4/5 h-48 bg-white text-lg">Slide 3</SwiperSlide>
-        <SwiperSlide className="flex justify-center items-center w-2/5 h-48 bg-white text-lg">Slide 4</SwiperSlide>
-        <SwiperSlide className="flex justify-center items-center w-4/5 h-48 bg-white text-lg">Slide 5</SwiperSlide>
-        <SwiperSlide className="flex justify-center items-center w-3/5 h-48 bg-white text-lg">Slide 6</SwiperSlide>
-        <SwiperSlide className="flex justify-center items-center w-4/5 h-48 bg-white text-lg">Slide 7</SwiperSlide>
-        <SwiperSlide className="flex justify-center items-center w-2/5 h-48 bg-white text-lg">Slide 8</SwiperSlide>
-        <SwiperSlide className="flex justify-center items-center w-4/5 h-48 bg-white text-lg">Slide 9</SwiperSlide>
+    <div className="p-6 bg-gray-100">
+      <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+        <SwiperSlide className="flex items-center justify-center bg-blue-500 text-white h-40">
+          Slide 1
+        </SwiperSlide>
+        <SwiperSlide className="flex items-center justify-center bg-green-500 text-white h-40">
+          Slide 2
+        </SwiperSlide>
+        <SwiperSlide className="flex items-center justify-center bg-red-500 text-white h-40">
+          Slide 3
+        </SwiperSlide>
+        {/* Add more slides as needed */}
       </Swiper>
     </div>
   );
